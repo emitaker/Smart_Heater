57c38bca4e5c415d7c74fb885c48de47a4d85611
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-11-17 18:51:57 -0600

    connects to a db

---
+++ connect_db.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------
+#
+# Emilio Campuzano Mejia
+#
+# In this code I simulate the connection part to the DB of the  
+# smart heater system
+#
+#-------------------------------------------------------------
+
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",#hostname where I'm going to connect
+        user=input("Enter username: "),#username where I'm goint to connect
+        password=getpass("Enter password: "),#user password
+        database="smart_heater",#DB name where I'm going to connect
+    ) as connection:
+        print(connection)
+except Error as e:
+    print(e)
+    
