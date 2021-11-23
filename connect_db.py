20dd54a600a99b982ab282f0a6571e01c4553130
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-11-22 22:29:45 -0600

    connection problem fixed

--- connect_db.py
+++ connect_db.py
@@ -12,10 +12,11 @@ from mysql.connector import connect, Error
 
 try:
     with connect(
-        host="localhost",#hostname where I'm going to connect
-        user="emitaker",#username where I'm goint to connect
+        host="192.168.100.7",#ip address of the server
+        port=3306,#db port
+        user="pi",#username of the db
         password="password",#user password
-        database="smart_heater",#DB name where I'm going to connect
+        database="smart_heater",#db name
     ) as connection:
         print(connection)
 except Error as e:
