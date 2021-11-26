cb970817233a37ec9875a7009c4acebf54b4bcc7
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-11-26 14:33:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,32 @@ from mysql.connector import connect, Error
 
 ser = serial.Serial('/dev/ttyACM0',9600)#serial port
 ser.flushInput() 
-ser_bytes = ser.readline()#reads incoming data from the serial port 
-decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")#extracts the important data
-print(decoded_bytes)
-
-try:
-    with connect(
-        host="192.168.100.9",#ip address of the server
-        port=3306,#db port
-        user="pi",#username of the db
-        password="password",#user password
-        database="smart_heater",#db name
-    ) as connection:
-        print(connection)
-        arduino_lec = 0
-        insert_temp_query = """
-        INSERT INTO temperatures (temperature)
-        VALUES
-            ({})
-        """.format(arduino_lec)
 
 
-        with connection.cursor() as cursor:
-            cursor.execute(insert_temp_query)
-            connection.commit()
+def main():
+    
+    while True:
 
-except Error as e:
-    print(e)
+        ser_bytes = ser.readline()#reads incoming data from the serial port 
+        decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")#extracts the important data
+        nice_bytes=float(decoded_bytes)
+        with connect(
+            host="192.168.100.9",#ip address of the server
+            port=3306,#db port
+            user="pi",#username of the db
+            password="password",#user password
+            database="smart_heater",#db name
+        ) as connection:
+            print(connection)
+            insert_temp_query = """
+            INSERT INTO temperatures (temperature)
+            VALUES
+                ({})
+            """.format(nice_bytes)
+            with connection.cursor() as cursor:
+                cursor.execute(insert_temp_query)
+                connection.commit()
+        
+        if nice_bytes == False:#no se si jala
+            connection.close()
+            break
