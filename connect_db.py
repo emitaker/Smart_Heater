f0836e25dfce4d9473b77aa5fc4359c25f23e959
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-11-25 20:46:37 -0600

    Update connect_db.py

--- connect_db.py
+++ connect_db.py
@@ -6,6 +6,8 @@
 # smart heater system
 #
 #-------------------------------------------------------------
+#CREATE USER 'pi'@'192.168.100.35' IDENTIFIED BY 'password';
+#GRANT ALL PRIVILEGES ON databasename.* TO 'pi'@'192.168.100.35';
 
 from getpass import getpass
 from mysql.connector import connect, Error
